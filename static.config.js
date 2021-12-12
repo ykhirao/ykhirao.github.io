5c09fa1bd9791de045e80bd0e800a995829e7888
Yuki <15008377+ykhirao@users.noreply.github.com>
2021-12-13 01:37:14 +0900

    run prittier

--- static.config.js
+++ static.config.js
@@ -1,58 +1,56 @@
-import axios from 'axios'
-import path from 'path'
-
-// const fs = require("fs").promises;
-import {promises as fs} from "fs";
+import axios from "axios";
+import path from "path";
+import { promises as fs } from "fs";
 
 // import { Post } from './types'
 
 // Typescript support in static.config.js is not yet supported, but is coming in a future update!
 // @ts-ignore
 export default {
-  entry: path.join(__dirname, 'src', 'index.tsx'),
+  entry: path.join(__dirname, "src", "index.tsx"),
   getRoutes: async () => {
-    console.log('#################')
+    console.log("#################");
     const posts = await getQiitaPosts();
     const about = await getAboutData();
     const events = await getConnpassEvents();
-    console.log('#################')
+    console.log("#################");
 
     const routes = [];
     if (posts) {
-      console.log('[routes] posts push');
+      console.log("[routes] posts push");
       routes.push({
-        path: '/posts',
+        path: "/posts",
         getData: () => ({
           posts,
         }),
         children: posts.map((post /* : Post */) => ({
           path: `/${post.id}`,
-          template: 'src/components/QiitaPost',
+          template: "src/components/QiitaPost",
           getData: () => ({
             post,
           }),
         })),
-      })
+      });
     }
     if (about) {
-      console.log('[routes] about push');
+      console.log("[routes] about push");
       routes.push({
-        path: '/about',
+        path: "/about",
         getData: () => ({
           about,
         }),
       });
       routes.push({
-        path: '/',
+        path: "/",
         getData: () => ({
           about,
         }),
-      })
+      });
     }
     if (events) {
-      console.log('[routes] events push');
+      console.log("[routes] events push");
       routes.push({
-        path: '/events',
+        path: "/events",
         getData: () => ({
           events: events.events,
         }),
@@ -62,57 +60,62 @@ export default {
     return routes;
   },
   plugins: [
-    'react-static-plugin-typescript',
+    "react-static-plugin-typescript",
     [
-      require.resolve('react-static-plugin-source-filesystem'),
+      require.resolve("react-static-plugin-source-filesystem"),
       {
-        location: path.resolve('./src/pages'),
+        location: path.resolve("./src/pages"),
       },
     ],
-    require.resolve('react-static-plugin-reach-router'),
-    require.resolve('react-static-plugin-sitemap'),
+    require.resolve("react-static-plugin-reach-router"),
+    require.resolve("react-static-plugin-sitemap"),
     [
-      'react-static-plugin-favicons',
-      { inputFile: path.resolve(__dirname, './src/assets/icon.png') },
+      "react-static-plugin-favicons",
+      { inputFile: path.resolve(__dirname, "./src/assets/icon.png") },
     ],
   ],
-}
+};
 
 async function getQiitaPosts() {
-  const cachePath = path.resolve('tmp/cache.qiita.json');
+  const cachePath = path.resolve("tmp/cache.qiita.json");
   if (await fileExists(cachePath)) {
-    console.log('[getQiitaPosts] GET Cache Data')
-    return JSON.parse((await fs.readFile(cachePath, "utf-8")));
+    console.log("[getQiitaPosts] GET Cache Data");
+    return JSON.parse(await fs.readFile(cachePath, "utf-8"));
   }
   const { data } /* :{ data: QiitaPost[] } */ = await axios.get(
-    'https://qiita.com/api/v2/users/ykhirao/items?page=1&per_page=100'
-  )
+    "https://qiita.com/api/v2/users/ykhirao/items?page=1&per_page=100"
+  );
   if (process.env.DEBUG === "TRUE") {
-    await fs.writeFile(cachePath, JSON.stringify(data, null, 4), 'utf-8')
+    await fs.writeFile(cachePath, JSON.stringify(data, null, 4), "utf-8");
   }
 
   return data;
 }
 
 async function getConnpassEvents() {
-  const connpassUsers = 'yk-hirao';
-  const connpassAPI = "https://connpass.com/api/v1/event/?order=2&count=40&nickname=" + connpassUsers + "&owner_nickname=" + connpassUsers
+  const connpassUsers = "yk-hirao";
+  const connpassAPI =
+    "https://connpass.com/api/v1/event/?order=2&count=40&nickname=" +
+    connpassUsers +
+    "&owner_nickname=" +
+    connpassUsers;
 
-  const cachePath = path.resolve('tmp/cache.connpass.json');
+  const cachePath = path.resolve("tmp/cache.connpass.json");
   if (await fileExists(cachePath)) {
-    console.log('[getConnpassEvents] GET Cache Data')
-    return JSON.parse((await fs.readFile(cachePath, "utf-8")));
+    console.log("[getConnpassEvents] GET Cache Data");
+    return JSON.parse(await fs.readFile(cachePath, "utf-8"));
   }
 
-  console.log('[getConnpassEvents] DEBUG FALSE')
-  const { data } /* :{ data: QiitaPost[] } */ = await axios.get(connpassAPI)
-    .catch(e => {
+  console.log("[getConnpassEvents] DEBUG FALSE");
+  const { data } /* :{ data: QiitaPost[] } */ = await axios
+    .get(connpassAPI)
+    .catch((e) => {
       console.log(e);
       throw new Error(e.message);
-    })
+    });
 
   if (process.env.DEBUG === "TRUE") {
-    await fs.writeFile(cachePath, JSON.stringify(data, null, 4), 'utf-8')
+    await fs.writeFile(cachePath, JSON.stringify(data, null, 4), "utf-8");
   }
   return data;
 }
@@ -124,4 +127,3 @@ async function getAboutData() {
 async function fileExists(filepath) {
   return !!(await fs.lstat(filepath).catch(() => false));
 }
-

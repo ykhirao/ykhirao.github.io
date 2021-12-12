4d51b624b21002609a7924417f6ee30b1d10646e
Yuki <15008377+ykhirao@users.noreply.github.com>
2021-12-12 15:28:47 +0900

    fix

---
+++ src/components/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Profile: React.FC = () => {
+  return (
+      <div>
+        {/*
+            @todo いい感じのプロフィール画面
+        */}
+      </div>
+  )
+}
+export default Profile;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { useRouteData } from 'react-static'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import Profile from 'components/Profile'
 
 export default () => {
   const { about }: { about: string } = useRouteData()
   // console.log(about);
   return (
     <>
+      <Profile />
       <ReactMarkdown children={about} className={'md-body'} remarkPlugins={[remarkGfm]} />
     </>
   )

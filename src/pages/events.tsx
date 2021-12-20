77e2a9482e3ffc92cbf8f74034c6e4d086d39375
Yuki <15008377+ykhirao@users.noreply.github.com>
2021-12-21 01:00:38 +0900

    fix event

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import { useRouteData } from 'react-static'
+import React from "react";
+import { useRouteData } from "react-static";
 
-import { ConnpassEvent } from 'types'
+import { ConnpassEvent } from "types";
 
 export default () => {
-  const { events }: { events: ConnpassEvent[] } = useRouteData()
+  const { events }: { events: ConnpassEvent[] } = useRouteData();
 
   return (
     <div>
-      <p>
-        All events:
-      </p>
+      <p>All events:</p>
       <ul>
-        {events && events.map && events.map(event => (
-          <li key={event.event_id}>
-            {event.started_at}{' '}
-            <a href={event.event_url}>{event.title} {event.catch}</a>
-            { event.hash_tag ? ` #${event.hash_tag }` : ''}
-          </li>
-        ))}
+        {events &&
+          events.map &&
+          events.map((event) => (
+            <div className="box">
+              <li key={event.event_id}>
+                {event.started_at}{" "}
+                <a href={event.event_url}>
+                  {event.title} {event.catch}
+                </a>
+                {event.hash_tag ? ` #${event.hash_tag}` : ""}
+              </li>
+              I'm in a box.
+            </div>
+          ))}
       </ul>
     </div>
-  )
-}
+  );
+};

0fe2cbe47f6faecd0fac374266d7fb2df8adb5e7
Yuki <15008377+ykhirao@users.noreply.github.com>
2021-12-21 01:41:45 +0900

    UPDATE events page

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import dayjs from "dayjs";
+import React, { useMemo, useState } from "react";
 import { useRouteData } from "react-static";
 
 import { ConnpassEvent } from "types";
@@ -6,24 +7,56 @@ import { ConnpassEvent } from "types";
 export default () => {
   const { events }: { events: ConnpassEvent[] } = useRouteData();
 
+  const [text, setText] = useState("");
+  const onChange = (e: any) => {
+    setText(e.target.value);
+  };
+
+  const filteredEvents = useMemo(() => {
+    console.log("use memo filteredEvents");
+    return (events || []).filter((event) => {
+      const isIncludeTitle = (event.title || "")
+        .toLocaleLowerCase()
+        .includes(text.toLocaleLowerCase());
+      const isIncludeCatch = (event.catch || "")
+        .toLocaleLowerCase()
+        .includes(text.toLocaleLowerCase());
+      return isIncludeTitle || isIncludeCatch;
+    });
+  }, [text]);
+
   return (
     <div>
+      <input
+        className="input is-normal"
+        type="text"
+        placeholder="Normal input"
+        onChange={onChange}
+      />
       <p>All events:</p>
       <ul>
-        {events &&
-          events.map &&
-          events.map((event) => (
-            <div className="box">
-              <li key={event.event_id}>
-                {event.started_at}{" "}
-                <a href={event.event_url}>
-                  {event.title} {event.catch}
-                </a>
-                {event.hash_tag ? ` #${event.hash_tag}` : ""}
-              </li>
-              I'm in a box.
-            </div>
-          ))}
+        {filteredEvents.map((event) => (
+          <div className="box" key={event.event_id}>
+            <article className="media">
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>{event.hash_tag}</strong>
+                    <small>
+                      {" "}
+                      in {dayjs(event.started_at).format(
+                        "YYYY/MM/DD HH:mm"
+                      )}{" "}
+                    </small>
+                    <small>~ {dayjs(event.ended_at).format("HH:mm")}</small>
+                    <br />
+                    <strong>{event.title}</strong> / {event.catch}
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+        ))}
       </ul>
     </div>
   );

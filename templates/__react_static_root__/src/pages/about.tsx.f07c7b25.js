bbfd2bcde311742be795c1e7f694d12f5bf2b18f
ykhirao <ykhirao@users.noreply.github.com>
2022-06-18 18:54:28 +0000

    deploy: ca71573e8aa65e1c83436aa393cf9c7f1e039ffd

--- templates/__react_static_root__/src/pages/about.tsx.f07c7b25.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{60:function(e,a,n){"use strict";n.r(a);var t=n(0),r=n.n(t),u=n(15),c=n(183),o=n(165);a.default=function(){var e=Object(u.useRouteData)().about;return r.a.createElement(r.a.Fragment,null,r.a.createElement(c.a,{children:e,className:"md-body",remarkPlugins:[o.a]}),",")}}}]);
---
+++ templates/styles.d47401f2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{140:function(n,o,w){},141:function(n,o,w){}}]);
